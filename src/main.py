89b2f96e0115ad263dcdb2cd3d12f9911935d29a
Paotic <dangnq.bi12-075@st.usth.edu.vn>
2023-04-02 10:59:44 +0700

    add GUI

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ from src.methods.DoctorMethods import DoctorMethods
 from src.methods.PatientMethods import PatientMethods
 from src.methods.RoomMethods import RoomMethods
 from src.methods.EmployeeMethods import EmployeeMethods
+
+
 import database
 
 def on_GUI_loaded():
@@ -55,14 +57,73 @@ pa_doc_list = []
 global pa_med_list
 pa_med_list = []
 
+
+def create_window(window):
+    new_window = Toplevel()
+    new_window.geometry("1000x800")
+    # new_window.minsize(1000, 800)
+    # new_window.maxsize(1000, 800)
+    new_window.title("Hospital Infolmation Management System")
+    new_window.config(bg="#319997")
+    icon = PhotoImage(file="C:\\Users\\dang1\\Pictures\\Presentation\Hospital_icon.png")
+    new_window.iconphoto(False, icon)
+    
+    with Image.open("C:\\Users\\dang1\\Pictures\\Presentation\Hospital_icon.png") as img:
+        Hos_icon = ImageTk.PhotoImage(img.resize((144,144)))
+
+
+    Frame(new_window, bg="#7C809B").place(x = 20, y = 20, width = 1000-40, height = 800-40)
+    Frame(new_window, bg="#ceede8").place(x = 22, y = 22, width = 1000-44, height = 800-44)
+    
+    Label(new_window, image=Hos_icon, bg="#ceede8", anchor="center").place(x=1000/2-68, y=1000/10)
+    Label.img = Hos_icon
+    
+    doctors_button = Button(new_window, text="DOCTORS", anchor="center", font=("Work Sans", 20), bg="#99CCCD", fg="white", relief="ridge", 
+            activebackground="#88C1C2", activeforeground="white", command=lambda: DoctorMethods.doc_press(window, doctors_list, patients_list, pa_doc_list))
+    doctors_button.place(x=100, y=800-500, width=1000-200, height = 50)
+    employees_button = Button(new_window, text="EMPLOYEES", anchor="center", font=("Work Sans", 20), bg="#99CCCD", fg="white", relief="ridge", 
+            activebackground="#88C1C2", activeforeground="white")
+    employees_button.place(x=100, y=800-440, width=1000-200, height = 50)
+    patients_button = Button(new_window, text="PATIENTS", anchor="center", font=("Work Sans", 20), bg="#99CCCD", fg="white", relief="ridge", 
+            activebackground="#88C1C2", activeforeground="white")
+    patients_button.place(x=100, y=800-380, width=1000-200, height = 50)
+    room_button = Button(new_window, text="ROOM", anchor="center", font=("Work Sans", 20),  bg="#99CCCD", fg="white", relief="ridge", 
+            activebackground="#88C1C2", activeforeground="white")
+    room_button.place(x=100, y=800-320, width=1000-200, height = 50)
+
+    
+    
+    
+
+#main window
 window = Tk()
+window.geometry("1200x800")
+window.minsize(1200, 800)
+window.maxsize(1200, 800)
 
-#TODO: GUI here
+window.title("Hospital Infolmation Management System")
+window.config(bg="#319997")
+icon = PhotoImage(file="C:\\Users\\dang1\\Pictures\\Presentation\Hospital_icon.png")
+window.iconphoto(False, icon)
 
-def main():
-    on_GUI_loaded()
-    window.protocol("WM_DELETE_WINDOW", on_exit)
-    window.mainloop()
+with Image.open("C:\\Users\\dang1\\Pictures\\Presentation\Hospital.png") as img:
+    Hos_Image = ImageTk.PhotoImage(img.resize((600,600)))
 
-if __name__ == "__main__":
-    main()
+
+    
+window.bind("<Button-1>", create_window)
+
+Label(window, image=Hos_Image, bg="#319997", anchor="center").pack()
+Label(window, text="""HOSPITAL INFORMATION 
+MANAGEMENT SYSTEM""", bg="#319997", fg="#e6edec", font=("Work Sans", 45, "bold")).pack()
+
+
+window.mainloop()
+
+# def main():
+#     on_GUI_loaded()
+#     window.protocol("WM_DELETE_WINDOW", on_exit)
+#     window.mainloop()
+
+# if __name__ == "__main__":
+#     main()
